7c99a0c81c8530c91f048336a485668e7215208f
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-18 20:19:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(number)
   if number < 0
     false
-  #elsif 
-  #  (2..(number-1)).each do |int| 
-  #    return false if number % int == 0
-  #  end
+  elsif 
+    (2..(number-1)).each do |int| 
+      return false if number % int == 0
+    end
   end
   #true
 end 
