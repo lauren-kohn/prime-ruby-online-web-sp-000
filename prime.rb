e5c1e83dfaff3786b783812877028871fb2ee0b3
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-18 20:15:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(number)
-  (2..(number-1)).each do |int| 
-    return false if number % int == 0
+  if number > 0
+    (2..(number-1)).each do |int| 
+      return false if number % int == 0
   end
   true
 end 
