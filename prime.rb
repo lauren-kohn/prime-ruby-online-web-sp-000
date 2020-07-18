d6a6d171cfb2d8bb91b57b7f567d39e019e20be9
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-18 20:08:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(number)
-  if number > 0
-    (2..number-1).none? {|int| number % int == 0}
-  else
-    false
-  end
+  number.prime?
+  #if number > 0
+  #  (2..number-1).none? {|int| number % int == 0}
+  #else
+  #  false
+  #end
 end 
