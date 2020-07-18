6ee1f2808f24aa8ace858e7bc140af80f8951533
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-18 20:33:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
 def prime?(num)
- n = 2
-  while n < num && num > 2
-    return true if num % n != 0
-    n += 1
+  if num < 2 
+    false
+  else
+    n = 2
+    while n < num && num > 2
+      return true if num % n != 0
+      n += 1
+    end
+    false
   end
-  false
 end
